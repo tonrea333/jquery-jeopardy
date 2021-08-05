d84050a516e47ce270fa092a152fb682976ed53a
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-08-05 15:58:11 -0400

    all

--- main.js
+++ main.js
@@ -1,36 +1,139 @@
 
-const tableMain = document.createElement("table")
-tableMain.setAttribute("class", "table");
-const tableHead = document.createElement("thead");
-tableHead.setAttribute("class", "Thead");
-const tableRow = document.createElement("tr");
-
-const tableHcol = document.createElement("th");
-tableH.setAttribute("scope", "col");
-
-const tableBody = document.createElement("tbody");
-
-const tableHrow = document.createElement("th");
-tableHrow.setAttribute("scope", "row");
+const pushButton = document.querySelector("#button");
+const bodyMaster = document.querySelector("#masterbody");
 
 
-const tableRow = document.createElement("tr");
-tableRow.setAttribute("class", "row");
-const tableHeader = document.createElement("th");
-tableHeader.setAttribute("class", "header");
-const tableData = document.createElement("td");
-tableData.setAttribute("class", "adata");
-const tableData2 = document.createElement("td");
-tableData2.setAttribute("class", "adata");
-const tabledataDiv = document.querySelector("#data")
+pushButton.addEventListener("click", onClick);
 
-tabledataDiv.appendChild(tableRow);
-tableRow.appendChild(tableHeader);
-tableHeader.append();
-tableRow.appendChild(tableData);
-tableData.append();
-tabledataDiv.appendChild(tableRow);
-tableRow.appendChild(tableData);
-tableData.append();
-tableRow.appendChild(tableData2);
-tableData2.append();
+function onClick(){
+
+
+
+const mainDiv = document.createElement("div");
+mainDiv.setAttribute("id", "mdiv");
+const boxDiv = document.createElement("div");
+boxDiv.setAttribute("class", "boxdiv");
+const boxDiv1 = document.createElement("div");
+boxDiv.setAttribute("class", "boxdiv1");
+const boxDiv2 = document.createElement("div");
+boxDiv.setAttribute("class", "boxdiv2");
+const boxDiv3 = document.createElement("div");
+boxDiv.setAttribute("class", "boxdiv3");
+const boxDiv4 = document.createElement("div");
+boxDiv.setAttribute("class", "boxdiv4");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bodyMaster.appendChild(mainDiv);
+mainDiv.append();
+mainDiv.appendChild(boxDiv);
+boxDiv.append("1");
+mainDiv.appendChild(boxDiv1);
+boxDiv1.append("2");
+mainDiv.appendChild(boxDiv2);
+boxDiv2.append("3");
+mainDiv.appendChild(boxDiv3);
+boxDiv3.append("4");
+mainDiv.appendChild(boxDiv4);
+boxDiv4.append("5");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const tableMain = document.createElement("table")
+//tableMain.setAttribute("class", "table");
+//const tableHead = document.createElement("thead");
+//tableHead.setAttribute("class", "Thead");
+//const tableRow = document.createElement("tr");
+//tableRow.setAttribute("class", "tr")
+//const tableHcol = document.createElement("th");
+//tableHcol.setAttribute("scope", "col");
+
+//const tableBody = document.createElement("tbody");
+
+//const tableHrow = document.createElement("th");
+//tableHrow.setAttribute("scope", "row");
+//const tableData = document.createElement("td");
+
+//bodyMaster.appendChild(tableMain);
+//tableMain.append();
+//tableMain.appendChild(tableRow);
+//tableRow.append();
+//tableRow.appendChild(tableHcol);
+//tableHcol.append("1");
+//tableRow.appendChild(tableHcol);
+//tableHcol.append("2");
+//tableRow.appendChild(tableHcol);
+//tableHcol.append("3");
+//tableRow.appendChild(tableHcol);
+//tableHcol.append("4");
+//tableRow.appendChild(tableHcol);
+//tableHcol.append("5");
+
+
+
+
+
+
+
+
+//const tableRow = document.createElement("tr");
+//tableRow.setAttribute("class", "row");
+//const tableHeader = document.createElement("th");
+//tableHeader.setAttribute("class", "header");
+//const tableData = document.createElement("td");
+//tableData.setAttribute("class", "adata");
+//const tableData2 = document.createElement("td");
+//tableData2.setAttribute("class", "adata");
+//const tabledataDiv = document.querySelector("#data")
+
+//tabledataDiv.appendChild(tableRow);
+//tableRow.appendChild(tableHeader);
+//tableHeader.append();
+//tableRow.appendChild(tableData);
+//tableData.append();
+//tabledataDiv.appendChild(tableRow);
+//tableRow.appendChild(tableData);
+//tableData.append();
+//tableRow.appendChild(tableData2);
+//tableData2.append();
+
+}
