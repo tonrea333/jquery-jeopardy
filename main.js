9e3f28d7d88199917b4a90c882ae7c505dcce75d
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-08-10 11:21:23 -0400

    all

--- main.js
+++ main.js
@@ -9,53 +9,323 @@ pushButton.addEventListener("click", jepBoxes);
 
 function jepBoxes() {
 
-
-
-
-
     const divMain = document.querySelector("#box")
-    for (let i = 0; i < 5; i++) {
-
-        let divRow = document.createElement("div");
-        divRow.setAttribute("id", "row");
-        divMain.appendChild(divRow)
-        let textRow = document.createTextNode("$100");
-        divRow.appendChild(textRow)
-
-        let divRow2 = document.createElement("div");
-        divRow2.setAttribute("id", "row2");
-        divMain.appendChild(divRow2);
-        let textRow2 = document.createTextNode("$200");
-        divRow2.appendChild(textRow2);
-
-        let divRow3 = document.createElement("div");
-        divRow3.setAttribute("id", "row3");
-        divMain.appendChild(divRow3);
-        let textRow3 = document.createTextNode("$400");
-        divRow3.appendChild(textRow3);
-
-        let divRow4 = document.createElement("div");
-        divRow4.setAttribute("id", "row4");
-        divMain.appendChild(divRow4)
-        let textRow4 = document.createTextNode("$600");
-        divRow4.appendChild(textRow4);
-
-        let divRow5 = document.createElement("div");
-        divRow5.setAttribute("id", "row5");
-        divMain.appendChild(divRow5);
-        let textRow5 = document.createTextNode("$800");
-        divRow5.appendChild(textRow5);
 
 
+    let divRow = document.createElement("div");
+    divRow.setAttribute("id", "row");
+    divMain.appendChild(divRow)
+    let textRow = document.createTextNode("$100");
+    divRow.appendChild(textRow)
 
+    let divRow2 = document.createElement("div");
+    divRow2.setAttribute("id", "row2");
+    divMain.appendChild(divRow2);
+    let textRow2 = document.createTextNode("$200");
+    divRow2.appendChild(textRow2);
 
+    let divRow3 = document.createElement("div");
+    divRow3.setAttribute("id", "row3");
+    divMain.appendChild(divRow3);
+    let textRow3 = document.createTextNode("$300");
+    divRow3.appendChild(textRow3);
 
+    let divRow4 = document.createElement("div");
+    divRow4.setAttribute("id", "row4");
+    divMain.appendChild(divRow4)
+    let textRow4 = document.createTextNode("$400");
+    divRow4.appendChild(textRow4);
 
+    let divRow5 = document.createElement("div");
+    divRow5.setAttribute("id", "row5");
+    divMain.appendChild(divRow5);
+    let textRow5 = document.createTextNode("$500");
+    divRow5.appendChild(textRow5);
+    //========================================================
+
+    let divRow6 = document.createElement("div");
+    divRow6.setAttribute("id", "row6");
+    divMain.appendChild(divRow6)
+    let textRow6 = document.createTextNode("$100");
+    divRow6.appendChild(textRow6)
+
+    let divRow7 = document.createElement("div");
+    divRow7.setAttribute("id", "row7");
+    divMain.appendChild(divRow7);
+    let textRow7 = document.createTextNode("$200");
+    divRow7.appendChild(textRow7);
+
+    let divRow8 = document.createElement("div");
+    divRow8.setAttribute("id", "row8");
+    divMain.appendChild(divRow8);
+    let textRow8 = document.createTextNode("$300");
+    divRow8.appendChild(textRow8);
+
+    let divRow9 = document.createElement("div");
+    divRow9.setAttribute("id", "row9");
+    divMain.appendChild(divRow9)
+    let textRow9 = document.createTextNode("$400");
+    divRow9.appendChild(textRow9);
+
+    let divRow10 = document.createElement("div");
+    divRow10.setAttribute("id", "row10");
+    divMain.appendChild(divRow10);
+    let textRow10 = document.createTextNode("$500");
+    divRow10.appendChild(textRow10);
+
+    //============================================================
+    let divRow11 = document.createElement("div");
+    divRow11.setAttribute("id", "row11");
+    divMain.appendChild(divRow11)
+    let textRow11 = document.createTextNode("$100");
+    divRow11.appendChild(textRow11)
+
+    let divRow12 = document.createElement("div");
+    divRow12.setAttribute("id", "row12");
+    divMain.appendChild(divRow12);
+    let textRow12 = document.createTextNode("$200");
+    divRow12.appendChild(textRow12);
+
+    let divRow13 = document.createElement("div");
+    divRow13.setAttribute("id", "row13");
+    divMain.appendChild(divRow13);
+    let textRow13 = document.createTextNode("$300");
+    divRow13.appendChild(textRow13);
+
+    let divRow14 = document.createElement("div");
+    divRow14.setAttribute("id", "row14");
+    divMain.appendChild(divRow14)
+    let textRow14 = document.createTextNode("$400");
+    divRow14.appendChild(textRow14);
+
+    let divRow15 = document.createElement("div");
+    divRow15.setAttribute("id", "row15");
+    divMain.appendChild(divRow15);
+    let textRow15 = document.createTextNode("$500");
+    divRow15.appendChild(textRow15);
+    //============================================================
+
+    let divRow16 = document.createElement("div");
+    divRow16.setAttribute("id", "row16");
+    divMain.appendChild(divRow16)
+    let textRow16 = document.createTextNode("$100");
+    divRow16.appendChild(textRow16)
+
+    let divRow17 = document.createElement("div");
+    divRow17.setAttribute("id", "row17");
+    divMain.appendChild(divRow17);
+    let textRow17 = document.createTextNode("$200");
+    divRow17.appendChild(textRow17);
+
+    let divRow18 = document.createElement("div");
+    divRow18.setAttribute("id", "row18");
+    divMain.appendChild(divRow18);
+    let textRow18 = document.createTextNode("$300");
+    divRow18.appendChild(textRow18);
+
+    let divRow19 = document.createElement("div");
+    divRow19.setAttribute("id", "row19");
+    divMain.appendChild(divRow19)
+    let textRow19 = document.createTextNode("$400");
+    divRow19.appendChild(textRow19);
+
+    let divRow20 = document.createElement("div");
+    divRow20.setAttribute("id", "row20");
+    divMain.appendChild(divRow20);
+    let textRow20 = document.createTextNode("$500");
+    divRow20.appendChild(textRow20);
+
+    //==============================================================
+    let divRow21 = document.createElement("div");
+    divRow21.setAttribute("id", "row21");
+    divMain.appendChild(divRow21)
+    let textRow21 = document.createTextNode("$100");
+    divRow21.appendChild(textRow21)
+
+    let divRow22 = document.createElement("div");
+    divRow22.setAttribute("id", "row22");
+    divMain.appendChild(divRow22);
+    let textRow22 = document.createTextNode("$200");
+    divRow22.appendChild(textRow22);
+
+    let divRow23 = document.createElement("div");
+    divRow23.setAttribute("id", "row23");
+    divMain.appendChild(divRow23);
+    let textRow23 = document.createTextNode("$300");
+    divRow23.appendChild(textRow23);
+
+    let divRow24 = document.createElement("div");
+    divRow24.setAttribute("id", "row24");
+    divMain.appendChild(divRow24)
+    let textRow24 = document.createTextNode("$400");
+    divRow24.appendChild(textRow24);
+
+    let divRow25 = document.createElement("div");
+    divRow25.setAttribute("id", "row25");
+    divMain.appendChild(divRow25);
+    let textRow25 = document.createTextNode("$500");
+    divRow25.appendChild(textRow25);
+    //========================================================    
+    //========================================================
+    const findRow = document.querySelector("#row");
+    findRow.addEventListener("click", oneHundred)
+    function oneHundred() {
 
 
 
     }
 
+    const findRow2 = document.querySelector("#row2");
+    findRow2.addEventListener("click", twoHundred)
+    function twoHundred() { }
+
+    const findRow3 = document.querySelector("#row3");
+    findRow3.addEventListener("click", fourHundred)
+    function fourHundred() { }
+
+    const findRow4 = document.querySelector("#row4");
+    findRow4.addEventListener("click", sixHundred)
+    function sixHundred() { }
+
+    const findRow5 = document.querySelector("#row5");
+    findRow5.addEventListener("click", eightHundred)
+    function eightHundred() { }
+
+    const findRow6 = document.querySelector("#row");
+    findRow6.addEventListener("click", oneHundred)
+    function oneHundred() { }
+
+    const findRow7 = document.querySelector("#row2");
+    findRow7.addEventListener("click", twoHundred)
+    function twoHundred() { }
+
+    const findRow8 = document.querySelector("#row3");
+    findRow8.addEventListener("click", fourHundred)
+    function fourHundred() { }
+
+    const findRow9 = document.querySelector("#row4");
+    findRow9.addEventListener("click", sixHundred)
+    function sixHundred() { }
+
+    const findRow10 = document.querySelector("#row5");
+    findRow10.addEventListener("click", eightHundred)
+    function eightHundred() { }
+
+    const findRow11 = document.querySelector("#row");
+    findRow11.addEventListener("click", oneHundred)
+    function oneHundred() { }
+
+    const findRow12 = document.querySelector("#row2");
+    findRow12.addEventListener("click", twoHundred)
+    function twoHundred() { }
+
+    const findRow13 = document.querySelector("#row3");
+    findRow13.addEventListener("click", fourHundred)
+    function fourHundred() { }
+
+    const findRow14 = document.querySelector("#row4");
+    findRow14.addEventListener("click", sixHundred)
+    function sixHundred() { }
+
+    const findRow15 = document.querySelector("#row5");
+    findRow15.addEventListener("click", eightHundred)
+    function eightHundred() { }
+
+    const findRow16 = document.querySelector("#row");
+    findRow16.addEventListener("click", oneHundred)
+    function oneHundred() { }
+
+    const findRow17 = document.querySelector("#row2");
+    findRow17.addEventListener("click", twoHundred)
+    function twoHundred() { }
+
+    const findRow18 = document.querySelector("#row3");
+    findRow18.addEventListener("click", fourHundred)
+    function fourHundred() { }
+
+    const findRow19 = document.querySelector("#row4");
+    findRow19.addEventListener("click", sixHundred)
+    function sixHundred() { }
+
+    const findRow20 = document.querySelector("#row5");
+    findRow20.addEventListener("click", eightHundred)
+    function eightHundred() { }
+
+    const findRow21 = document.querySelector("#row");
+    findRow21.addEventListener("click", oneHundred)
+    function oneHundred() { }
+
+    const findRow22 = document.querySelector("#row2");
+    findRow22.addEventListener("click", twoHundred)
+    function twoHundred() { }
+
+    const findRow23 = document.querySelector("#row3");
+    findRow23.addEventListener("click", fourHundred)
+    function fourHundred() { }
+
+    const findRow24 = document.querySelector("#row4");
+    findRow24.addEventListener("click", sixHundred)
+    function sixHundred() { }
+
+    const findRow25 = document.querySelector("#row5");
+    findRow25.addEventListener("click", eightHundred)
+    function eightHundred() { }
+
+//Random generator
+    function randomNumber() {
+        const x = document.querySelector("#questions");
+
+       
+
+
+        let y = Math.random()
+        let xe = Math.floor(y * 10000)
+        let showTime = Math.ceil(xe/100)*100
+
+
+
+
+        async function main() {
+            const rawData = await fetch("jeopardy.json");
+            const data = await rawData.json();
+            console.log('data', data[xe], data, xe, data[100].showNumber)
+    
+
+            let dataNum = data[showTime]
+            console.log(showTime)
+            console.log(dataNum)
+            console.log(dataNum.value)
+
+for(let i = 0; i < 99; i++){
+   let b =  dataNum.value[i]
+   console.log(b)
+}
+
+           
+    //======================================================       
+           
+            console.log(Math.round(y * 10000), y);
+    console.log(Math.ceil(xe),"xe")
+   
+   
+    
+
+
+
+            //Random generator called
+        }main()
+    } randomNumber();
+//==========================================================
+
+
+//Function to source jeopardy questions
+
+    
+
+
+
+    //====================================================
+    //================================================
 }
 
 
@@ -83,6 +353,16 @@ function jepBoxes() {
 
 
 
+//=========================================================
+//=========================================================
+//=========================================================
+
+
+
+//Things that did not provide a solution
+
+
+
 
 
 //const jepHeader = document.createElement("h1")
@@ -107,7 +387,62 @@ function jepBoxes() {
 //boxDiv.setAttribute("class", "boxdiv4");
 
 
+//for (let i = 0; i < 5; i++) {
 
+    //let divRow = document.createElement("div");
+    //divRow.setAttribute("id", "row");
+    //divMain.appendChild(divRow)
+    //let textRow = document.createTextNode("$100");
+    //divRow.appendChild(textRow)
+
+   // let divRow2 = document.createElement("div");
+   // divRow2.setAttribute("id", "row2");
+    //divMain.appendChild(divRow2);
+    //let textRow2 = document.createTextNode("$200");
+    //divRow2.appendChild(textRow2);
+
+   // let divRow3 = document.createElement("div");
+    //divRow3.setAttribute("id", "row3");
+    //divMain.appendChild(divRow3);
+   // let textRow3 = document.createTextNode("$400");
+    //divRow3.appendChild(textRow3);
+
+   // let divRow4 = document.createElement("div");
+    //divRow4.setAttribute("id", "row4");
+    //divMain.appendChild(divRow4)
+    //let textRow4 = document.createTextNode("$600");
+    //divRow4.appendChild(textRow4);
+
+    //let divRow5 = document.createElement("div");
+    //divRow5.setAttribute("id", "row5");
+    //divMain.appendChild(divRow5);
+    //let textRow5 = document.createTextNode("$800");
+    //divRow5.appendChild(textRow5);
+
+
+//}
+//const findRow = document.querySelector("#row");
+
+//findRow.addEventListener("click", oneHundred)
+//function oneHundred() {
+ //   console.log("one")
+ //}
+
+//const findRow2 = document.querySelector("#row2");
+//findRow2.addEventListener("click", twoHundred)
+//function twoHundred() { }
+
+//const findRow3 = document.querySelector("#row3");
+//findRow3.addEventListener("click", fourHundred)
+//function fourHundred() { }
+
+//const findRow4 = document.querySelector("#row4");
+//findRow4.addEventListener("click", sixHundred)
+//function sixHundred() { }
+
+//const findRow5 = document.querySelector("#row5");
+//findRow5.addEventListener("click", eightHundred)
+//function eightHundred() { }
 
 
 
